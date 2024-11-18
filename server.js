38b0f0d28cd3a04b7838bd1498065c8b8f805a99
howtobefun <duytdn2806@gmail.com>
2024-11-18 22:22:31 +0700

    First commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const ejs = require('ejs');
+
+const app = express();
+
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+
+// Serve static files
+app.use(express.static('public'));
+
+// Render the index page
+app.get('/', (req, res) => {
+  const data = {
+    title: 'SSR Web',
+    message: 'This is a dynamic message from the server'
+  };
+  res.render('index', data);
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
