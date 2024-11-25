0dc0c918bf21767322ee2666d4419872e1428d63
ntpd164 <duongngo1616@gmail.com>
2024-11-25 15:02:36 +0700

    Config tailwind css and live reload

---
+++ src/readme.txt
@@ -0,0 +1,2 @@
+Lưu ý khi code frontend:
+    Cần chạy song song 2 lệnh là npm run build:css và npm run dev
--- src/server.js
+++ src/server.js
@@ -1,23 +1,46 @@
-import express from 'express';
-import ejs from 'ejs';
+import express from "express";
+import ejs from "ejs";
+import livereload from "livereload";
+import connectLiveReload from "connect-livereload";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Tạo __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const liveReloadServer = livereload.createServer();
+liveReloadServer.watch([
+  path.join(__dirname, "views"),
+  path.join(__dirname, "public"),
+]);
 
 const app = express();
 
 // Set EJS as the view engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // Serve static files
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, "public")));
+
+// Add livereload to the middleware stack
+app.use(connectLiveReload());
+
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 100);
+});
 
 // Render the index page
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   const data = {
-    title: 'SSR Web',
-    message: 'This is a dynamic message from the server'
+    title: "SSR Web",
+    message: "This is a dynamic message from the server",
   };
-  res.render('index', data);
+  res.render("HomePage", data);
 });
 
 app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+  console.log("Server listening on port 3000");
 });
